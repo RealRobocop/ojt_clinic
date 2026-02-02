b034b7ac2a228a85fd3313510698b22698d7a5df
etsetera29 <jaylrnc@gmail.com>
2026-02-02 09:20:14 +0800

    no delete use archive instead

--- index.php
+++ index.php
@@ -250,7 +250,7 @@ $doctor_name = isset($_SESSION['doctor_name']) ? $_SESSION['doctor_name'] : 'Doc
                                                                 <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
                                                             </svg>
                                                         </button>
-                                                        <button class="btn-table danger" title="Delete" onclick="deletePatient(<?php echo $patient['id']; ?>)">
+                                                        <button class="btn-table danger" title="Archive" onclick="deletePatient(<?php echo $patient['id']; ?>)">
                                                             <svg style="width: 16px; height: 16px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                                                                 <polyline points="3 6 5 6 21 6"></polyline>
                                                                 <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
